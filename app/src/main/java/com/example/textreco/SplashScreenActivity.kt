bacfd653d7968447d118fc0a29d1e54d5fb6fcf6
Manjiri 1101 <Manjiri1101@users.noreply.github.com>
2021-05-17 23:12:39 -0700

    new UI with splash page

--- app/src/main/java/com/example/textreco/SplashScreenActivity.kt
+++ app/src/main/java/com/example/textreco/SplashScreenActivity.kt
@@ -4,18 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.core.os.HandlerCompat.postDelayed
 
 class SplashScreenActivity : AppCompatActivity() {
-    private val SPLASH_TIME : Long =3000
+    private val SPLASHTIME : Long = 1500
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-//        Handler.postDelayed(
-//        {
+
+        Handler(Looper.getMainLooper()).postDelayed(
+        {
         startActivity(Intent (this,MainActivity::class.java))
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
             finish()
-//        }, SPLASH_TIME)
+        }, SPLASHTIME)
     }
 }
